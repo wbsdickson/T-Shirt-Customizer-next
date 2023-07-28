582218dc8016cf81324c11cb23a1c95cf6a28860
Dickson Wu <wbs.dickson@gmail.com>
2023-07-28 16:08:01 -0600

    update tittle

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'T-Shirt customizer',
+  title: 'T-Shirt Customizer',
   description: '',
 }
 
